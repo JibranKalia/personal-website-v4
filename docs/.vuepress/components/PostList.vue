28c342b3e6183b2a66a9391e3a2956e105b5e59c
Jibran Kalia <jibran.kalia@gmail.com>
2020-04-21 11:01:26 -0500

    Adding

--- docs/.vuepress/components/PostList.vue
+++ docs/.vuepress/components/PostList.vue
@@ -13,8 +13,11 @@ export default {
       const posts = this.$site.pages
         .filter(p => p.path && p.path.startsWith(path) && p.path !== path)
         .sort((a, b) => {
-          return new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+          let b_date = b.frontmatter.date || b.lastUpdated;
+          let a_date = a.frontmatter.date || a.lastUpdated;
+          return new Date(b_date) - new Date(a_date)
         });
+      debugger;
       return posts;
     }
   }
---
+++ docs/knowledge/git-change-remote-url/index.md
@@ -0,0 +1,12 @@
+---
+tags:
+  - Git
+---
+
+# Change Git Remote Url
+
+```shell
+git remote set-url origin https://github.com/JibranKalia/personal-website-v4.git
+```
+
+[Source](https://help.github.com/en/github/using-git/changing-a-remotes-url)
