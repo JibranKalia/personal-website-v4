506fe3570a318869c08f8dc36eea79c3269b37f9
Jibran Kalia <jibran.kalia@gmail.com>
2020-09-29 07:46:47 -0500

    adding sitemap back

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,11 @@ module.exports = {
   title: "Jibran Kalia",
   description: 'Just playing around',
   plugins: [
+    [ 'sitemap', { hostname: 'https://jibrankalia.com' }],
     [ '@vuepress/search', { searchMaxSuggestions: 10 }, ],
     [ '@vuepress/search', { hostname: 'https://jibrankalia.com' } ],
     [ '@vuepress/google-analytics', { 'ga': 'UA-129311280-1' } ],
-    ['@limdongjin/vuepress-plugin-simple-seo', {
+    [ '@limdongjin/vuepress-plugin-simple-seo', {
       root_url: 'https://jibrankalia.com',
       default_site_name: "Jibran Kalia's Blog",
       twitter_creator: "@jibrankalia"
